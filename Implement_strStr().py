bdd7626cf029346274060d4bae3eeabeb8a7a476
James Crisp IV <97354178+james-crisp@users.noreply.github.com>
2022-08-02 11:47:54 -0400

    Create Implement_strStr().py
    Solution successful and faster and uses less memory than 80% of submissions.

---
+++ Implement_strStr().py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  2 11:43:13 2022
+
+@author: jamescrisp
+"""
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        
+        return haystack.find(needle)

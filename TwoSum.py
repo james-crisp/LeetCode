9cf29c591febe91bb3a0213d9d0862a4ffc922e5
James Crisp IV <97354178+james-crisp@users.noreply.github.com>
2022-08-05 10:57:18 -0400

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,23 +1,24 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        from itertools import combinations
+        locations = [0,0]
+        done = False
         
-        res = list(combinations(nums, 2))
-        #2,7 - 2,11 - 2,15 ... etc
-        #res[0][0] = 2
-        sol = [0,0]
-        for i in res:
-            if (i[0] + i[1]) == target:
-                if i[0] == i[1]:
-                    sol = [nums.index(i[0],0), nums.index(i[0],nums.index(i[0],0)+1)]
-                    return sol
-                else:
-                    sol = [nums.index(i[0]),nums.index(i[1])]
-                    return sol
-        
-        return "No solution"
-                    
+        def tryit(numbers):
+            test = numbers[0]
+            for j in range(1,len(numbers)):
+                if test + numbers[j] == target:
+                    locations[1] = j
+                    done = True
+                    return
+            
+        if done == True:
+            print(locations)
+            return locations
+        else:
+            for i in range(len(nums)):
+                locations[0] = i
+                tryit(nums[i:])
+
             
             
-        

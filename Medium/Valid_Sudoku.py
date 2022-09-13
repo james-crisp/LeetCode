c7ae1e106130478511e1a1808b64140eb65693dc
James Crisp IV <97354178+james-crisp@users.noreply.github.com>
2022-09-12 20:06:44 -0400

    Create Valid_Sudoku.py

---
+++ Medium/Valid_Sudoku.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 12 20:06:20 2022
+
+@author: jamescrisp
+"""
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        #1,2,3,4,5,6,7,8,9
+        array = [0,0,0,0,0,0,0,0,0] 
+        
+        def checkRows(sudo):
+            for i in range(len(sudo)):
+                temparray = array.copy()
+                
+                for j in sudo[i]:
+                    
+                    if isinstance(j,int):
+                        temparray[int(j)-1] += 1
+                        if temparray[int(j)-1] > 1:
+                            return False
+            return True
+        
+        if checkRows(board) == False:
+            return False
+        
+        def checkColumns(sudo):
+            for i in range(len(sudo)):
+                temparray = array.copy()
+                
+                for j in range(len(sudo)):
+                    #print(sudo[j][i])
+                    #print(temparray)
+                    if isinstance(sudo[j][i],int):
+                        print(temparray)
+                        temparray[int(sudo[j][i])-1] += 1
+                        if temparray[int(sudo[j][i])-1] > 1:
+                            return False
+            return True
+        
+        print(checkColumns(board))
+        if checkColumns(board) == False:
+            return False
+        
+        return True

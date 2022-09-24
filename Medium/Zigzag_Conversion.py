0c2f023b953d10a10e4c383cb1324b58164eb11b
James Crisp IV <97354178+james-crisp@users.noreply.github.com>
2022-09-24 18:24:34 -0400

    Create Zigzag_Conversion.py

---
+++ Medium/Zigzag_Conversion.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 24 18:22:36 2022
+
+@author: jamescrisp
+"""
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        length_word = len(s)
+        
+        final_word = [s[0]]
+        
+        #First Row
+        
+        x = numRows*2 - 2
+        
+        while x < length_word:
+            final_word.append(s[x])
+            x = x*2
+        
+        #Second Row
+        
+        x = 1
+        
+        while x < length_word:
+            
+        return

252a6c05d404d122bd9b76dee11693276003b737
James Crisp IV <97354178+james-crisp@users.noreply.github.com>
2022-10-06 19:04:17 -0400

    Moved problem from easy to medium.
    Edited problem as well.

--- Medium/LongestSubstringWithoutRepeatingCharacters.py
+++ Medium/LongestSubstringWithoutRepeatingCharacters.py
@@ -1,22 +1,21 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         total = 0
-        stack = []
+        stack = [s[0]]
         
         #create a push/pull stack
+        #recursion?
         
-        for i in range(0,len(s)):
-            for j in stack:
-                if j == s[i]:
-                    print(stack)
-                    if len(stack) > total:
-                        total = len(stack)
-                    stack = []
+        for i in range(1,len(s)):
             stack.append(s[i])
+            if s[i] in stack:
+                for j in range(len(stack)):
+                    if stack[j] == s[i]:
+                        stack = stack[j+1:]
+                    print(stack)
             if len(stack) > total:
                 total = len(stack)
                 
         
         return total
-            
         

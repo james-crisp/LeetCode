0bf9760b8c80e82338fb2ae68405fcb788eb821d
James Crisp IV <97354178+james-crisp@users.noreply.github.com>
2022-10-08 21:27:45 -0400

    Update Remove_Duplicates_from_Sorted_Array.py

--- Easy/Remove_Duplicates_from_Sorted_Array.py
+++ Easy/Remove_Duplicates_from_Sorted_Array.py
@@ -20,16 +20,17 @@ class Solution:
         
         for i in range(len(nums)-1):
             k = 1
-            count += 1
+            
             while nums[i] == nums[i+k]:
                 if k+i == len(nums) - 1:
                     break
                 nums[i+k] = nums[i] + 1
                 k += 1
-                
+            
             if k+i == len(nums) - 1:
                 break
             nums[i+1] = nums[i] + 1
+            count += 1
         '''
         count = 1
         for i in range(len(nums)-1):
@@ -39,6 +40,7 @@ class Solution:
         '''
         return count
             
+        
         
             
         

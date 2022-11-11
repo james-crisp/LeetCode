5fcb616d9b280c4b48e7a71634a0baf62e14b01d
James Crisp IV <97354178+james-crisp@users.noreply.github.com>
2022-11-10 21:05:49 -0500

    Update Combination_Sum.py

--- Medium/Combination_Sum.py
+++ Medium/Combination_Sum.py
@@ -9,6 +9,8 @@ Created on Wed Sep 14 17:32:21 2022
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
+        #DFS
+        
         total = 0
         num_List = []
         
@@ -28,4 +30,15 @@ class Solution:
                 #break
         
         return num_List
+    
+    def dfs(self,nums,target,path,ret):
+        if target < 0:
+            return
+        if target == 0:
+            ret.append([])
+            return
+        for i in range(len(nums)):
+            return self.dfs(nums[i:],target,path,ret)
+    
+                
                 

334abbf100c13aa083e715d6675e38400fc1a8ca
James Crisp IV <97354178+james-crisp@users.noreply.github.com>
2022-11-18 21:12:05 -0500

    Create Find_First_and_Last_Position_of_Element_in_Sorted_Array.py

---
+++ Medium/Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 18 21:11:38 2022
+
+@author: jamescrisp
+"""
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        
+        #binary search
+        
+        low = 0
+        high = len(nums) - 1
+        mid = high+low // 2
+        answer = []
+        prev = 0
+        
+        while mid != prev:
+            prev = mid
+            mid = high+low // 2
+            
+            if nums[mid] > target:
+                low = mid
+            else:
+                high = mid
+        
+        answer.append(mid)
+        answer.append(0)
+        
+        return answer
+                

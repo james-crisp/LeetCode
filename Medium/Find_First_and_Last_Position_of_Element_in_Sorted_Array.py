c720853dac0f2faabb673cd2b925066bc4fa1b05
James Crisp IV <97354178+james-crisp@users.noreply.github.com>
2022-11-22 21:39:01 -0500

    Update Find_First_and_Last_Position_of_Element_in_Sorted_Array.py

--- Medium/Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ Medium/Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -10,48 +10,41 @@ class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         
         #binary search
+        #separate left and right?
         
-        low = 0
-        mid = 0
-        high = len(nums) - 1
-        answer = []
-        
-        if len(nums) == 0:
-            return [-1,-1]
-        if len(nums) == 1:
-            if nums[0] == target:
-                return [0,0]
-        if len(nums) == 2:
-            if nums[low] == target and nums[high] == target:
-                return [0,1]
-            elif nums[low] == target:
-                return [0,0]
-            elif nums[high] == target:
-                return [1,1]
+        def bnsleft(nums1,target1):
+            low = 0
+            high = len(nums) - 1
+            mid = 0
             
-        while nums[mid] != target:
-            temp = mid
-            mid = (high+low) // 2
-            if nums[mid] == target:
-                break
-            elif temp == mid:
-                break
-            elif nums[mid] > target:
-                high = mid
-            else:
-                low = mid
+            while nums1 != target1:
+                prev = mid
+                mid = (low+high) // 2
+                
+                if prev == mid:
+                    mid = -1
+                    break
+                if nums1[mid] > target1:
+                    high = mid
+                else:
+                    low = mid
+            return mid
+            
+            
+        def bnsright(nums1,target1):
+            prev = mid
+                mid = (low+high) // 2
+                
+                if prev == mid:
+                    mid = -1
+                    break
+                if nums1[mid] > target1:
+                    high = mid
+                else:
+                    low = mid
+            return mid
         
-        if nums[mid] != target:
-            return [-1,-1]
-        
-        answer.append(mid)
-        while nums[mid] == target:
-            mid += 1
-            if mid == len(nums):
-                break
-        answer.append(mid-1)
-        
-        return answer
+        return 
                 
                 
                 

d69ae3e50bb1658cf2e496c32af1a07868d72671
James Crisp IV <97354178+james-crisp@users.noreply.github.com>
2022-11-27 19:30:54 -0500

    Create Concatenation_of_Array.py

---
+++ Medium/Concatenation_of_Array.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 27 19:30:16 2022
+
+@author: jamescrisp
+"""
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        
+        return nums + nums

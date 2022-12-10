c3d73b9f5725f945286b123abab74c91b631fa3b
James Crisp IV <97354178+james-crisp@users.noreply.github.com>
2022-12-10 12:23:00 -0500

    Update Permutations.py

--- Medium/Permutations.py
+++ Medium/Permutations.py
@@ -9,23 +9,23 @@ Created on Sat Nov 26 20:32:45 2022
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
-        def permutations(sets,n):
-            if n == 0:
-                return [[]]
+        def permutations(lst):
+            if len(lst) == 0:
+                return []
+            if len(lst) == 1:
+                return [lst]
             
             l = []
-            for i in range(0, len(sets)):
-                m = sets[i]
-                remLst = sets[i+1:]
+            for i in range(len(lst)):
+                m = lst[i]
+                remLst = lst[:i] + lst[i+1:]
                 
-                remainlst_combo = permutations(remLst,n-1)
-                print(remainlst_combo)
-                for p in remainlst_combo:
-                    l.append([m, *p])
-                print(l)
+                for p in permutations(remLst):
+                    l.append([m] + p)
+                
             return l
 
-        answer = permutations([x for x in nums],len(nums))
+        answer = permutations([x for x in nums])
         
 
         return answer

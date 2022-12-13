f28485e3c51933e802c2074839b4e4a1a57f1a2e
James Crisp <97354178+james-crisp@users.noreply.github.com>
2022-12-13 18:45:34 -0500

    Update Spiral_Matrix.py

--- Medium/Spiral_Matrix.py
+++ Medium/Spiral_Matrix.py
@@ -6,28 +6,37 @@ class Solution:
 
         spiral_order = []
         row_right = 0
-        row_left = len(matrix[0])-1
+        row_left = len(matrix[0]) - 1
         col_down = 0
-        col_up = len(matrix)
+        col_up = len(matrix) - 1
 
         while row_right < inf:
             #Traverse right
-            if row_right < len(matrix[0]):
+            if row_right <= row_left:
                 spiral_order.append(matrix[col_down][row_right])
                 row_right += 1
+                #Last row_right will be out of range
             #Traverse down
-            elif col_down < len(matrix):
+            elif col_down <= col_up:
                 spiral_order.append(matrix[col_down][row_right])
                 col_down += 1
+                #Last col_down will be out of range for next time
             #Traverse left
-            elif row_left > 0:
-                spiral_order.append(matrix[col_up][row_left])
+            elif row_left >= 0:
+                spiral_order.append(matrix[col_up][row_left - 1])
                 row_left -= 1
+            #Traverse up
             elif col_up > 0:
                 spiral_order.append(matrix[col_up][row_left])
                 col_up -= 1
         
         return spiral_order
+
+
+
+
+                
+
 
 
 

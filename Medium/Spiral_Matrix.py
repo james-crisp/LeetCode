d36f2e04f3e28f43751c33cc3915255e2b4bfa70
James Crisp <97354178+james-crisp@users.noreply.github.com>
2022-12-13 18:26:35 -0500

    Creating Spiral_Matrix.py

---
+++ Medium/Spiral_Matrix.py
@@ -0,0 +1,35 @@
+#Dec 13, 2022
+#James Crisp
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+
+        spiral_order = []
+        row_right = 0
+        row_left = len(matrix[0])-1
+        col_down = 0
+        col_up = len(matrix)
+
+        while row_right < inf:
+            #Traverse right
+            if row_right < len(matrix[0]):
+                spiral_order.append(matrix[0][row_right])
+                row_right += 1
+            #Traverse down
+            elif col_down < len(matrix):
+                spiral_order.append(matrix[col_down][len(matrix[0])])
+                col_down += 1
+            #Traverse left
+            elif row_left > 0:
+                spiral_order.append(matrix[len(matrix)][row_left])
+                row_left -= 1
+            elif col_up > 0:
+                spiral_order.append(matrix[col_up][0])
+                col_up -= 1
+        
+        return spiral_order
+
+
+
+
+                

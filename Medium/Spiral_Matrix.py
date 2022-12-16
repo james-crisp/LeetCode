9c54810462398a1293516529dd90f5d072650a42
James Crisp <97354178+james-crisp@users.noreply.github.com>
2022-12-16 13:02:28 -0500

    Update Spiral_Matrix.py

--- Medium/Spiral_Matrix.py
+++ Medium/Spiral_Matrix.py
@@ -15,23 +15,32 @@ class Solution:
             if row_right <= row_left:
                 spiral_order.append(matrix[col_down][row_right])
                 row_right += 1
-                #Last row_right will be out of range
+                
             #Traverse down
             elif col_down < col_up:
                 col_down += 1
                 spiral_order.append(matrix[col_down][row_right-1])
-                #Last col_down will be out of range for next time
+                
             #Traverse left
-            elif row_left >= 0:
+            elif row_left > 0:
                 spiral_order.append(matrix[col_up][row_left - 1])
                 row_left -= 1
+                
             #Traverse up
             elif col_up > 0:
                 col_up -= 1
-                spiral_order.append(matrix[col_up][row_left-1])
+                spiral_order.append(matrix[col_up][row_left])
+                print(row_left)
+                print(row_right)
             print(spiral_order)
         
         return spiral_order
+
+
+
+
+                
+
 
 
 

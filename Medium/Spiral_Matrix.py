6449496dea2fd75ce3a27631cc2e7eef2d10b1b5
James Crisp <97354178+james-crisp@users.noreply.github.com>
2022-12-18 13:27:15 -0500

    Clean up 'for' loops ranges
    Cleaned up 'for' loops to pass more test cases. Changed the ranges of the 'for' loops.

--- Medium/Spiral_Matrix.py
+++ Medium/Spiral_Matrix.py
@@ -12,27 +12,33 @@ class Solution:
 
         while row_right <= row_left and col_down <= col_up:
             #Traverse right
-            for i in range(row_left):
+            for i in range(row_right,row_left+1):
                 spiral_order.append(matrix[row_right][i])
             row_right += 1
                 
             #Traverse down
-            for i in range(col_up):
+            for i in range(col_down+1,col_up):
                 spiral_order.append(matrix[i][row_left])
             col_down += 1
-                
+            
             #Traverse left
-            for i in range(row_left):
+            for i in range(row_right-1,row_left):
                 spiral_order.append(matrix[col_up][-i-1])
             row_left -= 1
-                
+            
             #Traverse up
-            for i in range(col_up):
+            for i in range(col_down-1,col_up):
                 spiral_order.append(matrix[-i-1][row_right-1])
-                print(spiral_order)
             col_up -= 1
-        
+            
+
         return spiral_order
+
+
+
+
+                
+
 
 
 

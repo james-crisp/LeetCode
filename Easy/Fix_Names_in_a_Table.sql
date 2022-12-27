c4aa8328f074745e589b9736a6fd864e014ee5d3
James Crisp <97354178+james-crisp@users.noreply.github.com>
2022-12-26 19:50:20 -0500

    Change SELECT upper and lower case concatenation
    Code successful.

--- Easy/Fix_Names_in_a_Table.sql
+++ Easy/Fix_Names_in_a_Table.sql
@@ -5,5 +5,6 @@
 
 # Write your MySQL query statement below
 
-SELECT user_id, UPPER(LEFT(name,1))+LOWER(SUBSTRING(name,2,LEN(name)))
-FROM Users;
+SELECT user_id, CONCAT(UPPER(LEFT(name,1)),LOWER(SUBSTRING(name,2))) as name
+FROM Users
+ORDER BY user_id;

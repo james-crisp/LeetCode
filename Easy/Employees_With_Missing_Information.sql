1c857cd9b0c5243299801fbc1a9ec490f012e310
James Crisp <97354178+james-crisp@users.noreply.github.com>
2023-01-03 23:33:32 -0500

    Change CASE to UNION of SELECTs
    Combine both tables and show which id's aren't on both tables.

--- Easy/Employees_With_Missing_Information.sql
+++ Easy/Employees_With_Missing_Information.sql
@@ -4,13 +4,13 @@
 # 1965. Employees With Missing Information
 
 # Write your MySQL query statement below
+SELECT T.employee_id
+FROM
+    (SELECT employee_id FROM Employees
+    UNION
+    SELECT employee_id FROM Salaries)
+AS T
+WHERE T.salary IS NULL or T.name IS NULL;
 
-# Write your MySQL query statement below
 
-SELECT
-    CASE WHEN Employees.employee_id != Salaries.employee_id
-        THEN Employees.employee_id
-    END
-    AS employee_id
-FROM Employees
-LEFT JOIN Salaries ON Employees.employee_id=Salaries.employee_id;
+

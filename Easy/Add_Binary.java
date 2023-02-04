39add970d5233665516377e4477ef789e8289d4b
jrcrisp <jrcrisp@ncsu.edu>
2023-02-03 22:25:35 -0500

    [Implement] Create Add_Binary.java
    Does not pass tests.

---
+++ Easy/Add_Binary.java
@@ -0,0 +1,21 @@
+/**
+ * Add two binary strings together and return the resultant 
+ * binary string.
+ * 
+ * @author James Crisp
+ */
+class Solution {
+    public String addBinary(String a, String b) {
+        // Carry value
+        int carry = 0;
+        // Length of string a
+        int len = a.length();
+        // Set len to length of b if it's less than a
+        if (b.length() < len) {
+            len = b.length();
+        }
+        for (int i = 0; i < len; i++) {
+            //
+        }
+    }
+}
